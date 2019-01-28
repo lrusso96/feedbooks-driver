5e98953293340ae653fc798238da337ed81e519b
Luigi <russo.1699981@studenti.uniroma1.it>
2019-01-28 16:16:34 +0100

    UTC parser

---
+++ src/main/java/lrusso96/feedbooks/driver/core/Utils.java
@@ -0,0 +1,11 @@
+package lrusso96.feedbooks.driver.core;
+
+import java.time.*;
+
+public class Utils
+{
+    public static LocalDate parseUTC(String date){
+        Instant instant = Instant.parse(date);
+        return LocalDateTime.ofInstant(instant, ZoneId.of(ZoneOffset.UTC.getId())).toLocalDate();
+    }
+}
